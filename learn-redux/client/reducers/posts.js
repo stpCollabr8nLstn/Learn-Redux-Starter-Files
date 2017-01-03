90c28b65ac89be773207a6049eddfdf59b025ee7
amrios <amrios2817@gmail.com>
2017-01-03 16:16:03 -0600

    add single photo and comments

--- learn-redux/client/reducers/posts.js
+++ learn-redux/client/reducers/posts.js
@@ -4,8 +4,19 @@
 // 2. copy of current state
 
 function posts (state = [], action) {
-  console.log(state, action)
-  return state
+  switch (action.type) {
+    case 'INCREMENT_LIKES' :
+      console.log('Incrementing Likes!!')
+      const i = action.index
+      // Return the updated state
+      return [
+        ...state.slice(0, i), // before the one being udpated
+        {...state[i], likes: state[i].likes + 1},
+        ...state.slice(i + 1) // after the one being updated
+      ]
+    default:
+      return state
+  }
 }
 
 export default posts
